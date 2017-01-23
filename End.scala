6a086eb8b2c117a143ec486bd5bb413458e51e6b
shivangi1015 <shivangi1015@gmail.com>
2017-01-23 11:56:51 +0530

    Update End.scala

--- End.scala
+++ End.scala
@@ -1,16 +1,15 @@
 class Endoflist
 {
-    def findEnd(list:List[Int],index: Int): (Int,Int) = {
-        if(list.length==1) (list.head,index) else findEnd(list.tail,index+1)
+    def findEnd(list:List[Int],index: Int,prev: Int): (Int,Int) = {
+        if(list.isEmpty) (prev,index-1) else findEnd(list.tail,index+1,list.head)
     }
-    
 }
 
 object End {
     def main(args: Array[String]) {
     val myobj=new Endoflist
     val list=List(9,8,7,6,5,4,3,2,1)
-    val (element,index)=(myobj.findEnd(list,0))
+    val (element,index)=(myobj.findEnd(list,0,0))
     println(s"Last value $element And its index $index")
    
     }
